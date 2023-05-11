9f5a9e5c1f43c831bdf3e6e6db14696f5dead809
bm-hai07 <bmhai2504@gmail.com>
2023-05-11 14:35:20 +0700

    finish

---
+++ src/DeclareVar.java
@@ -0,0 +1,10 @@
+public class DeclareVar {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5d;
+        boolean b = true;
+        String s = "Ha Noi";
+        System.out.println(i + " " + f + " " + d + " " + b + " " + s);
+    }
+}
